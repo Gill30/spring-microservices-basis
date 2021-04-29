caf67ffc945fefa108f43a6133bf12d69f3d23f1
Ahmad Gill <gillahmad94.gmail.com>
2021-04-29 18:03:17 +0500

    currency conversion and currency exchange services, with feign client

---
+++ currency-conversion-service/currency-conversion-service/src/main/java/com/microservices/currencyconversionservice/feignProxy/CurrencyExchangeProxy.java
@@ -0,0 +1,16 @@
+package com.microservices.currencyconversionservice.feignProxy;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.microservices.currencyconversionservice.DTO.CurrencyConversion;
+
+@FeignClient(name="currency-exchange", url="localhost:8000")
+public interface CurrencyExchangeProxy {
+	
+	@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	public CurrencyConversion retrieveExcehangeValue(
+			@PathVariable String from,
+			@PathVariable String to) ;
+}
